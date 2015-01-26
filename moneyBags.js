5d39307e904ed5a528434b970a47489856b3ca1f
Brandon Burns <brandon10688@gmail.com>
2015-01-26 13:54:39 -0800

    site is functional with graph

---
+++ moneyBags.js
@@ -0,0 +1,63 @@
+function test() {
+	console.log($('#input1').val());
+}
+
+
+$('#test').on('click', test);
+
+$(function () {
+
+    $(document).ready(function () {
+
+        // Build the chart
+        $('#container').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            legend: {
+            align: 'right',
+            verticalAlign: 'top',
+            layout: 'vertical',
+            x: -50,
+            y: 130
+        	},
+
+            title: {
+                text: 'How You Are Spending Your Money'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Expense %',
+                data: [
+                    ['Mortgage',   45],
+                    ['IE',       26.8],
+                    {
+                        name: 'Chrome',
+                        y: 12.8,
+                        sliced: true,
+                        selected: true
+                    },
+                    ['Safari',    8.5],
+                    ['Opera',     6.2],
+                    ['Others',   0.7]
+                ]
+            }]
+        });
+    });
+
+});
